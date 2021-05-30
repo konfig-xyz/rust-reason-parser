50fe293136af72a4bda6f76ce9406794b7e5db46
Roland Peelen <roland@peppr.nl>
2021-05-30 11:48:01 +0200

    :sparkles: - Parse Schema

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# rust-reason
+# Rust Diesel -> ReasonML 
+
+## Before First Run
+Pre-requisites: 
+- [Haskell Stack](https://docs.haskellstack.org/en/stable/README/)
+
+## Development
+
+- Build
+    ```bash
+    stack build
+    ```
+- Build & Run
+    ```bash
+    stack run
+    ```
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,40 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (someFunc) where
+
+import Text.Parsec hiding (count)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  contents <- readFile "schema.rs"
+  print $ parseSchema contents
+
+type TypePair = (String, String)
+
+type Table = (String, [TypePair])
+
+type Schema = [Table]
+
+typeTuple :: Parsec String () TypePair
+typeTuple = do
+  spaces
+  typeName <- manyTill anyChar $ spaces *> string "->" <* spaces
+  typeVar <- manyTill anyChar $ string ","
+  optional eof
+  return (typeName, typeVar)
+
+table :: Parsec String () Table
+table = do
+  string "table! {" <* try spaces
+  string "use diesel::sql_types::*;" <* try spaces
+  typeName <- manyTill anyChar $ try space
+  spaces
+  try $ manyTill anyChar $ try $ string "{"
+  contents <- manyTill (try typeTuple) $ try $ spaces *> string "}"
+  spaces
+  try $ string "}"
+  return (typeName, contents)
+
+schema :: Parsec String () Schema
+schema = manyTill (try table <* spaces) $ try $ string "joinable"
+
+--parseSchema :: String -> String
+parseSchema = runParser schema () "Err"
