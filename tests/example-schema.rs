d22ff7a2676a0bdfc19c2d6f59a11e93ad088312
Roland Peelen <roland@peppr.nl>
2024-05-09 23:34:52 +0200

    :bug: - Fix parsing super::sql_types::*

--- tests/example-schema.rs
+++ tests/example-schema.rs
@@ -9,45 +9,48 @@ pub mod sql_types {
 diesel::table! {
     use diesel::sql_types::*;
 
-    hide_me (hide_me_id) {
-        hide_me_id -> Uuid,
+    companies (id) {
+        id -> Uuid,
+        company_name -> Text,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        archived_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::Role;
+
+    members (id) {
+        id -> Uuid,
+        user_id -> Uuid,
+        company_id -> Uuid,
+        role -> Role,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        archived_at -> Nullable<Timestamp>,
     }
 }
 
 diesel::table! {
     use diesel::sql_types::*;
 
-    qualified_shown (test_id) {
-        qualified_field -> Text,
+    users (id) {
+        id -> Uuid,
+        email -> Text,
+        password -> Text,
+        blocked -> Bool,
+        is_super_user -> Bool,
+        verified -> Bool,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        archived_at -> Nullable<Timestamp>,
     }
 }
 
-diesel::table! {
-    use diesel::sql_types::*;
+diesel::joinable!(members -> companies (company_id));
+diesel::joinable!(members -> users (user_id));
 
-    qualified_hide (test_id) {
-        qualified_field -> Text,
-        another_qualified_field -> Text,
-    }
-}
+diesel::allow_tables_to_appear_in_same_query!(companies, members, users,);
 
-diesel::table! {
-    use diesel::sql_types::*;
-
-    test (test_id) {
-        test_id -> Uuid,
-        hidden_id -> Uuid,
-        some_string -> Text,
-        some_bool -> Bool,
-        some_int -> Int4,
-        some_float -> Float4,
-        some_array -> Array<Text>,
-        some_option -> Nullable<Text>,
-    }
-}
-
-diesel::joinable!(foo -> bar (bar_id));
-
-diesel::allow_tables_to_appear_in_same_query!(
-    bar,
-);
