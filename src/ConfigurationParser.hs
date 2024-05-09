723964d16f9237ba623e2c7c0ffc2508c552bca5
Roland Peelen <roland@peppr.nl>
2024-05-09 22:38:09 +0200

    :art: - Support Rescript Syntax

--- src/ConfigurationParser.hs
+++ src/ConfigurationParser.hs
@@ -1,17 +1,23 @@
-{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module ConfigurationParser (makeConfig) where
 
-import           Data.Bifunctor   (first)
-import qualified Data.Map         as M
-import           Data.Maybe       (mapMaybe)
-import qualified Data.Set         as S
-import qualified Data.Text        as T
-import           Yaml (Config, keys, load, lookup, lookupDefault,
-                                   subconfig)
-import           Helpers          (parseTypeSplitBy)
-import qualified Types            as T
+import Data.Bifunctor (first)
+import qualified Data.Map as M
+import Data.Maybe (mapMaybe)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import Helpers (parseTypeSplitBy)
+import qualified Types as T
+import Yaml
+  ( Config,
+    keys,
+    load,
+    lookup,
+    lookupDefault,
+    subconfig,
+  )
 
 parseTypeMapConfiguration :: T.Text -> Maybe (T.Text, T.Text)
 parseTypeMapConfiguration = parseTypeSplitBy "->"
@@ -22,8 +28,10 @@ toTypeMap = M.fromList . mapMaybe parseTypeMapConfiguration
 toQualified :: Config -> T.HiddenQualified
 toQualified xs = M.fromList $ map (\k -> (k, S.fromList $ lookupDefault k [] xs)) (keys xs)
 
-toPPXs :: [T.Text] -> [T.Text]
-toPPXs = map (\x -> "[@" <> x <> "]\n")
+toLanguage :: T.Text -> T.Language
+toLanguage "reason" = T.Reason
+toLanguage "rescript" = T.Rescript
+toLanguage _ = T.Reason
 
 makeConfig :: String -> IO T.Configuration
 makeConfig path = do
@@ -35,9 +43,10 @@ makeConfig path = do
 
   pure $
     T.Configuration
-      (toPPXs $ lookupDefault "alias-ppx" [] annotations)
-      (toPPXs $ lookupDefault "type-ppx" [] annotations)
-      (toPPXs $ lookupDefault "containerized-ppx" [] annotations)
+      (toLanguage $ lookupDefault "language" "reason" config)
+      (lookupDefault "alias-ppx" [] annotations)
+      (lookupDefault "type-ppx" [] annotations)
+      (lookupDefault "containerized-ppx" [] annotations)
       (toTypeMap $ lookupDefault "aliases" [] types)
       (toTypeMap $ lookupDefault "containerized" [] types)
       (toTypeMap $ lookupDefault "base" [] types)
